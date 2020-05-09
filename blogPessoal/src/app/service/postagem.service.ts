dde748a4f8e1cb4ce247721a1c43332ef981cc40
Otavio Mendes <otecnopro.gm@hotmail.com>
2020-05-08 21:03:50 -0300

    Criação do endPoint getAllPostagens

--- blogPessoal/src/app/service/postagem.service.ts
+++ blogPessoal/src/app/service/postagem.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostagemService {
 
-  constructor() { }
+  // todo modulo criado colocar dentro do parenteses do construtor
+  constructor( private http: HttpClient) { }
+
+  /* 
+  
+  CRUD - Create = get, Ready = post, Update = put e Delete = delete
+  */
+
+  // importar modulo de HTTP
+
+getAllPostagens()
+{
+  return this.http.get('http://31.220.57.14:8080/postagens')
+}
 }
